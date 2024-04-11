dacbac1e785f7972b516087f47a4de6dd7635439
S1zenly <semaclasss@mail.ru>
2024-04-12 00:52:53 +0300

    final version with daemon

--- rest-Server/src/main/java/ru/hse/softwear/cinemaworld/userServer/service/SchedulerService.java
+++ rest-Server/src/main/java/ru/hse/softwear/cinemaworld/userServer/service/SchedulerService.java
@@ -1,7 +1,10 @@
 package ru.hse.softwear.cinemaworld.userServer.service;
 
 import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Service;
 import ru.hse.softwear.cinemaworld.userServer.service.scheduler.ClearSessionTokenScheduler;
@@ -10,9 +13,12 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Service
 public class SchedulerService implements ApplicationListener<ApplicationReadyEvent> {
 
+    @Autowired
+    private ApplicationContext context;
     private static final int count = 2;
     private static final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(count);
 
@@ -22,7 +28,8 @@ public class SchedulerService implements ApplicationListener<ApplicationReadyEve
     }
 
     public void run() {
-        executorService.scheduleAtFixedRate(new ClearSessionTokenScheduler(), 0, 20, TimeUnit.MINUTES);
+        log.info("Daemon-threads starts");
+        executorService.scheduleAtFixedRate(new ClearSessionTokenScheduler(context), 0, 20, TimeUnit.MINUTES);
     }
 
     @PreDestroy
--- rest-Server/src/main/java/ru/hse/softwear/cinemaworld/userServer/service/scheduler/ClearSessionTokenScheduler.java
+++ rest-Server/src/main/java/ru/hse/softwear/cinemaworld/userServer/service/scheduler/ClearSessionTokenScheduler.java
@@ -1,6 +1,9 @@
 package ru.hse.softwear.cinemaworld.userServer.service.scheduler;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import ru.hse.softwear.cinemaworld.userServer.cypher.OrderTokenCypher;
 import ru.hse.softwear.cinemaworld.userServer.service.RedisService;
@@ -9,14 +12,21 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Set;
 
+@Slf4j
 @Component
 public class ClearSessionTokenScheduler implements Runnable {
 
+
+    private final RedisService redisService;
+
     @Autowired
-    private RedisService redisService;
+    public ClearSessionTokenScheduler(ApplicationContext context) {
+        this.redisService = context.getBean(RedisService.class);
+    }
 
     @Override
     public void run() {
+        log.info("Start clear order thread");
         Set<String> tokens = redisService.allKeysInCacheOrderSession();
 
         for(String token : tokens) {
